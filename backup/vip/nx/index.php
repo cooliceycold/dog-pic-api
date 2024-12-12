cca753536ea9a8c3fb5e8b6f02363c4d6abaf12e
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2024-12-12 13:09:08 +0800

    Add files via upload

--- backup/vip/nx/index.php
+++ backup/vip/nx/index.php
@@ -1,1 +1,26 @@
-<?php $a=file('img.txt');$b=$a[array_rand($a)];header("Location:$b");
+<?php
+function get_data($file) {
+    $lines = file($file, FILE_IGNORE_NEW_LINES);
+    return $lines[array_rand($lines)];
+}
+
+function generate_url($data, $prefixes, $suffix) {
+    $prefix = $prefixes[array_rand($prefixes)];
+    return $prefix . $data . $suffix;
+}
+
+$file = 'img.txt'; // 你的txt文件名
+$data = get_data($file);
+$prefixes = [
+        'https://doglink.cf/gh/cooliceycold/pic/vip/nx/',
+        'https://fastly.doglink.cf/gh/cooliceycold/pic/vip/nx/',
+        'https://jscdn.doglink.cf/gh/cooliceycold/pic/vip/nx/',
+        'https://gcore.jsdelivr.net/gh/cooliceycold/pic/vip/nx/',
+        'https://testingcf.jsdelivr.net/gh/cooliceycold/pic/vip/nx/',
+        'https://gcore.doglink.cf/gh/cooliceycold/pic/vip/nx/',
+    ];
+$suffix = '.webp';
+$url = generate_url($data, $prefixes, $suffix);
+
+header("Location: $url");
+?>
--- backup/vip/xq/index.php
+++ backup/vip/xq/index.php
@@ -1,1 +1,26 @@
-<?php $a=file('img.txt');$b=$a[array_rand($a)];header("Location:$b");
+<?php
+function get_data($file) {
+    $lines = file($file, FILE_IGNORE_NEW_LINES);
+    return $lines[array_rand($lines)];
+}
+
+function generate_url($data, $prefixes, $suffix) {
+    $prefix = $prefixes[array_rand($prefixes)];
+    return $prefix . $data . $suffix;
+}
+
+$file = 'img.txt'; // 你的txt文件名
+$data = get_data($file);
+$prefixes = [
+        'https://doglink.cf/gh/cooliceycold/pic/vip/xq/',
+        'https://fastly.doglink.cf/gh/cooliceycold/pic/vip/xq/',
+        'https://jscdn.doglink.cf/gh/cooliceycold/pic/vip/xq/',
+        'https://gcore.jsdelivr.net/gh/cooliceycold/pic/vip/xq/',
+        'https://testingcf.jsdelivr.net/gh/cooliceycold/pic/vip/xq/',
+        'https://gcore.doglink.cf/gh/cooliceycold/pic/vip/xq/',
+    ];
+$suffix = '-tuya.webp';
+$url = generate_url($data, $prefixes, $suffix);
+
+header("Location: $url");
+?>
